59da8036464ee592e8b51a7fb163b05749dd3712
JHscar <wogns5206@naver.com>
2025-07-11 00:52:40 +0900

    eas test

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function SettingScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>Setting screen</Text>
     </View>
   );
 }
@@ -11,11 +11,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
+    backgroundColor: "#e2e2e2",
     justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    color: "#fff",
+    color: "#000",
   },
 });
